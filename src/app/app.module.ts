8cdbdaa7671206ec7766b925a6f162665d49f4fe
vinashinde <shinde.vina3@gmail.com>
2018-06-25 16:34:24 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { PeopleListComponent } from './people-list/people-list.component';
+import { appRouterModule } from './app.routes';
+import { PersonDetailsComponent } from './person-details/person-details.component';
+import { PeopleService } from './people.service';
+import { MaxValidatorDirective } from './max-validator.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PeopleListComponent,
+    PersonDetailsComponent,
+    MaxValidatorDirective
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    appRouterModule
+  ],
+  providers: [PeopleService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/people.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Person } from './person';
+
+const PEOPLE : Person[] = [
+      {id: 1, name: 'Luke Skywalker', height: 177, weight: 70},
+      {id: 2, name: 'Darth Vader', height: 200, weight: 100},
+      {id: 3, name: 'Han Solo', height: 185, weight: 85},
+    ];
+
+@Injectable()
+export class PeopleService{
+
+  getAll() : Person[] {
+    return PEOPLE.map(p => this.clone(p));
+  }
+  get(id: number) : Person {
+    return this.clone(PEOPLE.find(p => p.id === id));
+  }
+  save(person: Person){
+    let originalPerson = PEOPLE.find(p => p.id === person.id);
+    if (originalPerson) Object.assign(originalPerson, person);
+    // saved moahahaha
+  }
+
+  private clone(object: any){
+    // hack
+    return JSON.parse(JSON.stringify(object));
+  }
+}
